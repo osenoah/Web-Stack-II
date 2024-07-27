47ab37a357ab280697b139dcb6239920c95c85e3
osenoah <noahoshose2@gmail.com>
2024-07-27 09:41:47 +0100

    update

---
+++ Quiz/quiz.js
@@ -0,0 +1,363 @@
+var questions = [
+    {
+        question: "Who was the first man created by God?" ,
+        answers: [
+            {text: "Moses", correct: false},
+            {text: "Adam", correct: true},
+            {text: "Abraham", correct: false},
+            {text: "Noah", correct: false},
+        ]
+    },
+    {
+        question: "Which book of the Bible contains the story of Noah and the Ark?" ,
+        answers: [
+            {text: "Exodus", correct: false},
+            {text: "Leviticus", correct: false},
+            {text: "Genesis", correct: true},
+            {text: "Numbers", correct: false},
+        ]
+    },
+    {
+        question: "Who led the Israelites out of Egypt?" ,
+        answers: [
+            {text: "Aaron", correct: false},
+            {text: "Joseph", correct: false},
+            {text: "Joshua", correct: false},
+            {text: "Moses", correct: true},
+        ]
+    },
+    {
+        question: "What did God create on the first day?" ,
+        answers: [
+            {text: "Stars", correct: false},
+            {text: "Sky", correct: false},
+            {text: "Light", correct: true},
+            {text: "The Seas", correct: false},
+        ]
+    },
+    {
+        question: "Who was the wife of Isaac?" ,
+        answers: [
+            {text: "Rebekah", correct: true},
+            {text: "Leah", correct: false},
+            {text: "Sarah", correct: false},
+            {text: "Rachel", correct: false},
+        ]
+    },
+    {
+        question: "Which city did God destroy because of its wickedness?" ,
+        answers: [
+            {text: "Babylon", correct: false},
+            {text: "Nineveh", correct: false},
+            {text: "Sodom", correct: true},
+            {text: "Jericho", correct: false},
+        ]
+    },
+    {
+        question: "What was the name of the giant slain by David?" ,
+        answers: [
+            {text: "Samson", correct: false},
+            {text: "Goliath", correct: true},
+            {text: "Saul", correct: false},
+            {text: "Ahab", correct: false},
+        ]
+    },
+    {
+        question: "In the Book of Daniel, what were the names of Daniel's three friends?" ,
+        answers: [
+            {text: "Shadrach, Moses, and Abednego", correct: false},
+            {text: "Samuel, Meshach, and Abednego", correct: false},
+            {text: "Shadrach, Meshach, and Abednego", correct: true},
+            {text: "Shadrach, Micah, and Abednego", correct: false},
+        ]
+    },
+    {
+        question: "Who was thrown in the Lion's Den?" ,
+        answers: [
+            {text: "Jonah", correct: false},
+            {text: "Ezekiel", correct: false},
+            {text: "Jethro", correct: false},
+            {text: "Daniel", correct: true},
+        ]
+    },
+    {
+        question: "Which woman was known for her loyalty to her mother-in-law Naomi?" ,
+        answers: [
+            {text: "Ruth", correct: true},
+            {text: "Esther", correct: false},
+            {text: "Deborah", correct: false},
+            {text: "Naomi", correct: false},
+        ]
+    },
+    {
+        question: "Which prophet confronted King Ahab and Queen Jezebel?" ,
+        answers: [
+            {text: "Elisha", correct: false},
+            {text: "Isaiah", correct: false},
+            {text: "Elijah", correct: true},
+            {text: "Jeremiah", correct: false},
+        ]
+    },
+    {
+        question: "What was the name of Abraham's first son?" ,
+        answers: [
+            {text: "Isaac", correct: false},
+            {text: "Esau", correct: false},
+            {text: "Laban", correct: false},
+            {text: "Ishmael", correct: true},
+        ]
+    },
+    {
+        question: "Who interpreted dreams for Pharaoh in Egypt?" ,
+        answers: [
+            {text: "Joshua", correct: false},
+            {text: "Jeremiah", correct: false},
+            {text: "Reuben", correct: false},
+            {text: "Joseph", correct: true},
+        ]
+    },
+    {
+        question: "What did God give the Israelites to eat in the wilderness?" ,
+        answers: [
+            {text: "Quail and Honey", correct: false},
+            {text: "Fish and Bread", correct: false},
+            {text: "Manna and Quail", correct: true},
+            {text: "Bread and Wine", correct: false},
+        ]
+    },
+    {
+        question: "Who was the last judge of Israel?" ,
+        answers: [
+            {text: "Samson", correct: false},
+            {text: "Eli", correct: false},
+            {text: "Samuel", correct: true},
+            {text: "Gideon", correct: false},
+        ]
+    },
+    {
+        question: "Which book is known as the book of wisdom?" ,
+        answers: [
+            {text: "Psalms", correct: false},
+            {text: "Ecclesiastes", correct: false},
+            {text: "Proverbs", correct: true},
+            {text: "Job", correct: false},
+        ]
+    },
+    {
+        question: "Who built the first temple in Jerusalem?" ,
+        answers: [
+            {text: "David", correct: false},
+            {text: "Solomon", correct: true},
+            {text: "Saul", correct: false},
+            {text: "Hezekiah", correct: false},
+        ]
+    },
+    {
+        question: "Who was the mother of Samuel?" ,
+        answers: [
+            {text: "Sarah", correct: false},
+            {text: "Bathsheba", correct: false},
+            {text: "Leah", correct: false},
+            {text: "Hannah", correct: true},
+        ]
+    },
+    {
+        question: "Which king had a dream about a statue made of different materials?" ,
+        answers: [
+            {text: "King Solomon", correct: false},
+            {text: "King David", correct: false},
+            {text: "King Nebuchadnezzar", correct: true},
+            {text: "King Saul", correct: false},
+        ]
+    },
+    {
+        question: "What was the name of Jacob's youngest son?" ,
+        answers: [
+            {text: "Reuben", correct: false},
+            {text: "Simon", correct: false},
+            {text: "Benjamin", correct: true},
+            {text: "Levi", correct: false},
+        ]
+    },
+    {
+        question: "In which city were the followers of Jesus first called Christians?" ,
+        answers: [
+            {text: "Jerusalem", correct: false},
+            {text: "Antioch", correct: true},
+            {text: "Corinth", correct: false},
+            {text: "Rome", correct: false},
+        ]
+    },
+    {
+        question: "Who wrote the majority of the New Testament letters?" ,
+        answers: [
+            {text: "Peter", correct: false},
+            {text: "John", correct: false},
+            {text: "James", correct: false},
+            {text: "Paul", correct: true},
+        ]
+    },
+    {
+        question: "What was the profession of Luke?" ,
+        answers: [
+            {text: "Physician", correct: true},
+            {text: "Tax collector", correct: false},
+            {text: "Fisherman", correct: false},
+            {text: "Priest", correct: false},
+        ]
+    },
+    {
+        question: "Who was the first martyr in the New Testament?" ,
+        answers: [
+            {text: "Peter", correct: false},
+            {text: "Matthias", correct: false},
+            {text: "Stephen", correct: true},
+            {text: "Simeon", correct: false},
+        ]
+    },
+    {
+        question: "Which apostle was exiled to the island of Patmos?" ,
+        answers: [
+            {text: "Peter", correct: false},
+            {text: "John", correct: true},
+            {text: "James", correct: false},
+            {text: "Matthew", correct: false},
+        ]
+    },
+    {
+        question: "Who was the Roman governor who sentenced Jesus to be crucified?" ,
+        answers: [
+            {text: "Pilate", correct: true},
+            {text: "Caesar", correct: false},
+            {text: "Herod", correct: false},
+            {text: "Pilus", correct: false},
+        ]
+    },
+    {
+        question: "Who was the first person to see the resurrected Jesus?" ,
+        answers: [
+            {text: "Mary, mother of Christ", correct: false},
+            {text: "Mary Magdalene", correct: true},
+            {text: "Thomas", correct: false},
+            {text: "Peter", correct: false},
+        ]
+    },
+    {
+        question: "Who was known as the 'Beloved Disciple'?" ,
+        answers: [
+            {text: "Peter", correct: false},
+            {text: "John", correct: true},
+            {text: "James", correct: false},
+            {text: "Judas", correct: false},
+        ]
+    },
+    {
+        question: "What is the 'Great Commission'?" ,
+        answers: [
+            {text: "Jesus' command to love one another", correct: false},
+            {text: "The call to pray without ceasing", correct: false},
+            {text: "The command to keep the Sabbath", correct: false},
+            {text: "The command to go and make disciples of all nations", correct: true},
+        ]
+    },
+    {
+        question: "Who was the Pharisee that visited Jesus at night?" ,
+        answers: [
+            {text: "Gamaliel", correct: false},
+            {text: "Nicodemus", correct: true},
+            {text: "Caiaphas", correct: false},
+            {text: "Joseph of Arimathea", correct: false},
+        ]
+    },
+    {
+        question: "Which gospel is traditionally believed to have been written first?" ,
+        answers: [
+            {text: "Mark", correct: true},
+            {text: "John", correct: false},
+            {text: "Matthew", correct: false},
+            {text: "Luke", correct: false},
+        ]
+    },
+    {
+        question: "Who wrote the Book of Revelation?" ,
+        answers: [
+            {text: "The Romans", correct: false},
+            {text: "John", correct: true},
+            {text: "James", correct: false},
+            {text: "Paul", correct: false},
+        ]
+    },
+    {
+        question: "Who was the high priest who presided over Jesus' trial?" ,
+        answers: [
+            {text: "Caiaphas", correct: true},
+            {text: "Annas", correct: false},
+            {text: "Pilate", correct: false},
+            {text: "Eli", correct: false},
+        ]
+    },
+    {
+        question: "Who was the first to preach to the Gentiles?" ,
+        answers: [
+            {text: "Paul", correct: false},
+            {text: "Simeon", correct: false},
+            {text: "James", correct: false},
+            {text: "Cephas", correct: true},
+        ]
+    },
+    {
+        question: "What did the angels tell the shepherds when Jesus was born?" ,
+        answers: [
+            {text: "'Peace on earth, goodwill to men'", correct: false},
+            {text: "'Glory to God in the highest'", correct: true},
+            {text: "'He is not here, for He is risen'", correct: false},
+            {text: "'Fear not, for behold, this is good news'", correct: false},
+        ]
+    },
+    {
+        question: "Which book describes the early church and the spread of the gospel?" ,
+        answers: [
+            {text: "Acts", correct: true},
+            {text: "Matthew", correct: false},
+            {text: "Mark", correct: false},
+            {text: "Luke", correct: false},
+        ]
+    },
+    {
+        question: "What did King Solomon ask God for?" ,
+        answers: [
+            {text: "Strength", correct: false},
+            {text: "Wealth", correct: false},
+            {text: "Wisdom", correct: true},
+            {text: "Battle Victory", correct: false},
+        ]
+    },
+    {
+        question: "Which prophet confronted the prophets of Baal on Mount Carmel?" ,
+        answers: [
+            {text: "Jeremiah", correct: false},
+            {text: "Elisha", correct: false},
+            {text: "Elijah", correct: true},
+            {text: "Eli", correct: false},
+        ]
+    },
+    {
+        question: "What was the name of the queen who visited King Solomon?" ,
+        answers: [
+            {text: "Queen of Sheba", correct: true},
+            {text: "Queen Esther", correct: false},
+            {text: "Queen Jezebel", correct: false},
+            {text: "Queen Vashti", correct: false},
+        ]
+    },
+    {
+        question: "Who denied Jesus three times before the rooster crowed?" ,
+        answers: [
+            {text: "Peter", correct: true},
+            {text: "John", correct: false},
+            {text: "James", correct: false},
+            {text: "Paul", correct: false},
+        ]
+    },
+
+]
