9606b0c0922f73141625e61bf04c519057bed931
osenoah <noahoshose2@gmail.com>
2024-09-23 17:15:19 +0100

    add

---
+++ Odin1/index.js
@@ -0,0 +1,8 @@
+const http = require('http');
+const url = require('url');
+const fs = require('fs');
+
+const quakers = './index.html'
+const err = './404.html'
+const about = './about.html'
+const contact = './contact-me.html'
