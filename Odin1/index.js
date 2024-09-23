06709ee89f96341289bc3e58debd12da8f65bfd7
osenoah <noahoshose2@gmail.com>
2024-09-23 17:46:10 +0100

    dn

--- Odin1/index.js
+++ Odin1/index.js
@@ -2,17 +2,18 @@ const http = require('http');
 const url = require('url');
 const fs = require('fs');
 
-const quakers = './index.html'
-const err = './404.html'
-const about = './about.html'
-const contact = './contact-me.html'
+// const quakers = './index.html'
+// const err = './404.html'
+// const about = './about.html'
+// const contact = './contact-me.html'
 
 const Quaker = http.createServer ( function (req,res) {
     const q = url.parse(req.url, true)
     const p = "." + q.pathname
-    fs.readFile(p, 'utf8', function (err, data){
-        if (err) {
-            console.error('error')
+    fs.readFile(p, 'utf8', function (error, data){
+        if (error) {
+            res.writeHead(404, {'Content-Type': 'text/html'});
+            return res.end("404 Not Found");
         }
         else {
             res.writeHead(200, {'Content-Type': 'text/html'})
@@ -23,4 +24,5 @@ const Quaker = http.createServer ( function (req,res) {
 }
 );
 
-Quaker.listen(8080)
+Quaker.listen(8080);
+console.log(`Service running at http://localhost:8080`)
