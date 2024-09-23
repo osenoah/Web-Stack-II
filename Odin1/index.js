f44b5b39987ed8836075b6d3cf60244f37c1bd32
osenoah <noahoshose2@gmail.com>
2024-09-23 17:28:11 +0100

    add Func

--- Odin1/index.js
+++ Odin1/index.js
@@ -6,3 +6,21 @@ const quakers = './index.html'
 const err = './404.html'
 const about = './about.html'
 const contact = './contact-me.html'
+
+const Quaker = http.createServer ( function (req,res) {
+    const q = url.parse(req.url, true)
+    const p = "." + q.pathname
+    fs.readFile(p, 'utf8', function (err, data){
+        if (err) {
+            console.error('error')
+        }
+        else {
+            res.writeHead(200, {'Content-Type': 'text/html'})
+            res.write(data)
+            return res.end()
+        }
+    })
+}
+);
+
+Quaker.listen(8080)
