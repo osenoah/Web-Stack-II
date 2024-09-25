5633d45981684d4a8e04b26130f302c5ca1187e2
osenoah <noahoshose2@gmail.com>
2024-09-25 09:52:32 +0100

    fix

--- Odin1/inexp.js
+++ Odin1/inexp.js
@@ -7,14 +7,17 @@ const upg = express();
 upg.get('/:filename', (req,res)=>{
     const filename = req.params.filename
     const file = path.join(__dirname, 'pages', filename)
+    console.log(req.params)
 
-    fs.access(file, (err) =>{
+// to access/open the files
+    fs.readFile(file, (err) =>{
         if (err) {
+            console.error(err)
             return res.sendFile(path.join(__dirname, 'pages', '404.html'))
         }
         res.sendFile(file)
     })
-    // res.sendFile(path.join(__dirname, 'pages', 'index.html'))
+
 })
 
 upg.listen(8080, () => console.log(`Service running at http://localhost:8080`));
