3c603d6e6d49bf5c3488ec5b21f4f675caf1a69a
osenoah <noahoshose2@gmail.com>
2024-09-25 07:36:55 +0100

    alter

--- Odin1/index.js
+++ Odin1/index.js
@@ -14,24 +14,6 @@ const upg = express();
 //     const q = url.parse(req.url, true);
 //     const p = "." + q.pathname;
 
-    upg.get('./Odin1', (req,res) =>{
-        const wtd = path.join(__dirname, 'Odin1')
-    
-        const Filename = req.params.name
-        res.sendFile(Filename, wtd, (err)=>{
-            if(err){
-                console.error(err)
-            } else{
-               console.log('Sent:', Filename) 
-            }
-        })
-
-
-
-
-
-
-    });
     // fs.readFile(p, "utf8", function (error, data) {
     //     if (error) {
     //         fs.readFile("./404.html", "utf8", function (err404, data404) {
@@ -50,6 +32,26 @@ const upg = express();
     //     }
     // });
 // });
+
+
+// upg.get('/Odin1', (req,res) =>{
+    //     const wtd = path.join(__dirname, 'Pages')
+    
+    //     const Filename = req.params.name
+    //     res.sendFile(Filename, wtd, function (err){
+    //         if(err){
+    //             console.error(err)
+    //         } else{
+    //            console.log('Sent:', Filename) 
+    //         }
+    //     })
+    // });
+
+    upg.get('/', (req,res)=>{
+        const ut = 'index.html'
+        res.sendFile(path.join(__dirname,'Pages', ut))
+    })
+
 
 upg.listen(8080, () => console.log(`Service running at http://localhost:8080`));
 
