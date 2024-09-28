53ccca27427b6fa3b73843f2e6d2529c31aa1bd6
osenoah <noahoshose2@gmail.com>
2024-09-28 09:38:24 +0100

    update contr

--- Task Manager/controllers/tasks.js
+++ Task Manager/controllers/tasks.js
@@ -1,8 +1,20 @@
-
-
-
 const getAllTasks = (req,res) => {
-    res.send('Bunny Hopping')
+    res.send('Get All Tasks')
 }
 
-module.exports = { getAllTasks }
+const createNewTask = (req,res) => {
+    res.send('Create New Task')
+}
+
+const getSingleTask = (req,res) => {
+    res.send('Get single task')
+}
+
+const updateTask = (req,res) => {
+    res.send('Update Task')
+}
+
+const deleteTask = (req,res) => {
+    res.send('Delete Task')
+}
+module.exports = { getAllTasks, createNewTask, getSingleTask, updateTask, deleteTask }
--- Task Manager/routers/tasks.js
+++ Task Manager/routers/tasks.js
@@ -1,7 +1,16 @@
 const express = require('express');
 const router = express.Router();
 const { getAllTasks } = require('../controllers/tasks')
+const { createNewTask } = require('../controllers/tasks')
+const { getSingleTask } = require('../controllers/tasks')
+const { updateTask } = require('../controllers/tasks')
+const { deleteTask } = require('../controllers/tasks')
+
 
 router.route('/').get(getAllTasks)
+router.route('/').post(createNewTask)
+roouter.route('/').get(getSingleTask)
+router.route('/').patch(updateTask)
+router.route('/').delete(deleteTask)
 
 module.exports = router;
