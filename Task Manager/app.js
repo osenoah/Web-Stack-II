8ae4d0a16a2507f7111c300a2fac035ad65aa119
osenoah <noahoshose2@gmail.com>
2024-09-28 08:55:41 +0100

    Jedi

--- Task Manager/app.js
+++ Task Manager/app.js
@@ -1,3 +1,12 @@
 const express = require('express')
 
-const fie = express();
+const app = express();
+
+//routes
+app.get('/hello', (req,res)=>{
+    res.send('Omo Node sha')
+})
+
+const port = 3000;
+
+app.listen(port, console.log(`Server is running on ${port}....`))
