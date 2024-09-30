cbe7d767564da3b7585d0b270c789cf3077c9263
osenoah <noahoshose2@gmail.com>
2024-09-30 14:53:47 +0100

    fix

--- Task Manager/models/tasks.js
+++ Task Manager/models/tasks.js
@@ -1,7 +1,16 @@
 const mongoose = require('mongoose')
 
 const TaskSchema = new mongoose.Schema({
-    name:String,completed:Boolean
+    name: {
+        type:String,
+        required:[true, 'Must Provide name'],
+        trim:true,
+        maxlength:[15, 'Name cannot be more than 15 Characters']
+    },
+    completed:{
+        type:Boolean,
+        default:false,
+    },
 })
 
 module.exports = mongoose.model('Task', TaskSchema)
