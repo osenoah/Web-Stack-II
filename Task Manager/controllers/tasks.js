cb670b82c7a6923c52398fc39945ee1dd006f0a8
osenoah <noahoshose2@gmail.com>
2024-10-02 11:22:14 +0100

    Odin Project II

--- Task Manager/controllers/tasks.js
+++ Task Manager/controllers/tasks.js
@@ -1,12 +1,22 @@
 const Task = require('../models/tasks')
 
-const getAllTasks = (req,res) => {
-    res.send('Get All Tasks')
+const getAllTasks = async (req,res) => {
+    try {
+        const tasks = await Task.find({})
+        res.status(200).json({ tasks })
+    }catch (err){
+        res.status(500).json({ msg: err })
+    }
 }
 
 const createNewTask = async (req,res) => {
-    const task = await Task.create(req.body)
-    res.status(200).json({ task })
+    try{
+        const task = await Task.create(req.body)
+        res.status(200).json({ task })
+    }catch(err){
+        res.status(500).json({ msg: err })
+    }
+    
 }
 
 const getSingleTask = (req,res) => {
